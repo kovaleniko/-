96bb5da2ba151248addf9db89cf6538896e65a27
kovaleniko <kovaleniko@yandex.ru>
2020-07-08 21:36:18 +0300

    add realisation

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -9,17 +9,25 @@
 
 int main()
 {
-   /* Sensor* p = new Adapter(new FahrenheitSensor);
-    cout << "Celsius temperature = " << p->getTemperature() << endl;
-    delete p;*/
+    //Sensor* p = new Adapter(new FahrenheitSensor);
+    //cout << "Celsius temperature = " << p->getTemperature() << endl;
+    //delete p;
 
+    //Generator* r = new RandomGeneratorAdapter;
+    //std::cout << "random " << r->next() << endl;
     /*SimpleRing r;
-    LockupAlarmClock bell(r,10,37);
-    bell.start();*/
-   
-    proxy_h();
-    //setlocale(LC_ALL, "");
+    LockupAlarmClock bell(r,19,34);
+    bell.start();
+   */
+    //proxy_h();
+    setlocale(LC_ALL, "");
     //facade();
     //composite();
+    //flyweight();
+    //Pizza* pz = new TomatoPizza(new ItalianPizza);
+    //std::cout<<pz->Name_get()<<std::endl;
+    //Pizza* bpz = new BulgerianPizza;
+    //bpz = new CheesePizza(bpz);
+    //std::cout << bpz->Name_get() << " " << bpz->GetCost() << std::endl;
     return 0;
 }
